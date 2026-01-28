dc73746cb8527447cf4ee5cc09b6d3b79aff6a89
arbi <msahelarbi@gmail.com>
2026-01-28 17:30:21 +0100

    update file

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Configurer le resolver pour supporter l'alias @/
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    '@': path.resolve(__dirname),
+  },
+};
 
 module.exports = config;
